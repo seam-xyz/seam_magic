4cee4fd1d07ba2e327cf5c76507f3c09abcb2552
Nick Confrey <nickconfrey@earthlink.net>
2024-07-10 13:10:59 -0400

    copy changes

--- app/LandingPageComponent.tsx
+++ app/LandingPageComponent.tsx
@@ -14,17 +14,14 @@ export const LandingPageComponent = ({ onSubmit }: { onSubmit: (userInput: strin
       </div>
       <div className="flex flex-col items-center justify-between">
         <div>
-          <h1 className="text-2xl font-bold mb-2">Welcome to the Seam Mini app Ai Builder</h1>
-          <p className="text-center text-gray-600 mb-4">
-            Create & Discover Miniapps For builders, designers, and curators
-          </p>
+          <h1 className="text-2xl font-bold mb-2">seam ✨ magic ✨ miniapp maker</h1>
         </div>
 
         <div>
           <h2 className="text-lg font-semibold mb-2 block">
             What miniapp do you want to create today?
           </h2>
-          <div className="relative">
+          <div className="items-center">
             <input
               type="text"
               placeholder="A watercolor mini app..."
@@ -32,10 +29,13 @@ export const LandingPageComponent = ({ onSubmit }: { onSubmit: (userInput: strin
               className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:border-blue-500"
               onChange={(e) => setInput(e.target.value)}
             />
+            <p className="mt-1 text-gray-400 text-xs">
+              Powered by <a href="https://www.anthropic.com/news/claude-3-5-sonnet" className="text-blue-400">Claude 3.5</a>
+            </p>
           </div>
           <button onClick={() => onSubmit(input)} type="submit"
             className="mt-4 w-full py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
-            Magic
+            cook
           </button>
         </div>
 
